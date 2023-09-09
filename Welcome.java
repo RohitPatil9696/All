98305263a00fbadad81e07ec1a3aa24dd2580dac
RohitPatil9696 <patilrj41998@gmail.com>
2023-09-10 01:03:07 +0530

    Tester update

--- Welcome.java
+++ Welcome.java
@@ -3,7 +3,9 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("Welcome to Bharat");
+     System.out.println("Welcome to Bharat"); 
+     System.out.println("I love My Country");
+     
 	}
 
 }
